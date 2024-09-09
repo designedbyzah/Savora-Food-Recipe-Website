f4864cf9ed1b5b497cd0e46d85dc75c228f4cfd6
designedbyzah <119671886+designedbyzah@users.noreply.github.com>
2024-09-09 02:07:19 +0100

    Second commit-Updated HTML, CSS and JS

--- backup.js
+++ backup.js
@@ -8,3 +8,31 @@ tabsContainer.addEventListener("click", (event) => {
 
   console.log(index);
 });
+
+//Check for input n forms
+forms.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = form.querySelector(".search-inputBar");
+    let userInput = input.value;
+    console.log(userInput);
+
+    form.reset();
+  });
+});
+
+// Function for Search Form Validation
+function searchInputValidation(formIndex) {
+  searchInputBars.forEach((searchInputBar, index) => {
+    if (index === formIndex) {
+      if (searchInputBar.value.trim() === "") {
+        errorValidationTexts[index].textContent = "Enter a search item";
+        return false;
+      } else {
+        errorValidationTexts[index].textContent = "";
+        return true;
+      }
+    }
+  });
+}
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const tabs = document.querySelectorAll(".tabs-container button");
 const resultContainers = document.querySelectorAll(".recipe-result-container");
-const headerSearchBar = document.getElementById("header-search-input");
+const errorValidationTexts = document.querySelectorAll(".form-validation-error-state");
 const headerSearchIconBtn = document.getElementById("header-search-icon-btn");
-const searchInput = querySelectorAll(".search-inputBar");
+const searchInputBars = document.querySelectorAll(".search-inputBar");
+const forms = document.querySelectorAll(".search-form");
 
-// Function to switch between Breakfast tabs
+// Function to switch between Breakfast/Lunch/Dinner/Dessert stabs
 function switchTabs() {
   tabs.forEach((tab, index) => {
     tab.addEventListener("click", () => {
@@ -27,11 +28,54 @@ headerSearchIconBtn.addEventListener("click", () => {
   headerSearchIconBtn.style.display = "none";
   if (headerSearchIconBtn) {
     headerSearchIconBtn.style.display = "none";
-    headerSearchBar.style.display = "grid";
-  } else if (headerSearchBar) {
+    forms[0].style.display = "flex";
+  } else if (forms[0]) {
     headerSearchIconBtn.style.display = "flex";
-    headerSearchBar.style.display = "none";
+    forms[0].style.display = "none";
   }
 });
 
+function searchInputValidation(index) {
+  if (searchInputBars[index].value.trim() === "") {
+    errorValidationTexts[index].textContent = "Enter a search item";
+    return false;
+  } else {
+    errorValidationTexts[index].textContent = "";
+    return true;
+  }
+}
+
 //Collect User value/query
+forms.forEach((form, index) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!searchInputValidation(index)) {
+      return;
+    }
+
+    let userInput = searchInputBars[index].value.trim();
+    foodRecipeData(userInput);
+
+    forms[0].style.display = "none";
+    headerSearchIconBtn.style.display = "flex";
+
+    form.reset();
+  });
+});
+
+//Spoonacular API
+async function foodRecipeData(userInput) {
+  const apiKEY = "b57c0eb865e84768954fa8ec016596b0";
+  const recipeEndpoint = `https://api.spoonacular.com/recipes/complexSearch?query=${userInput}&apiKey=${apiKEY}&number=2`;
+
+  try {
+    const response = await fetch(recipeEndpoint);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error("HTTP Error 404 or 405");
+    }
+    console.log(data);
+  } catch (error) {
+    console.log("Error fecting item data");
+  }
+}
